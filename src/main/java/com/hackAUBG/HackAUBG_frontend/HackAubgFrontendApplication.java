98adc3a22940aa0e1c153cae2c1bb1bf67f1e6d9
Belis Aliosmanova <benidebelis@gmail.com>
2024-12-12 21:03:21 +0200

    Initial commit

---
+++ src/main/java/com/hackAUBG/HackAUBG_frontend/HackAubgFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.hackAUBG.HackAUBG_frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackAubgFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackAubgFrontendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackAUBG/HackAUBG_frontend/HackAubgFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackAUBG.HackAUBG_frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackAubgFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
